e7cd63ce3eed9511cb00d4c75e86d73cb35984e5
Sairam Chekuri <sairamc3@gmail.com>
2024-02-25 14:45:33 +0530

    Adding more logic to Coing Change problem

--- dynamic_programming/CoinChangeProblem/CoinChangeProblem.java
+++ dynamic_programming/CoinChangeProblem/CoinChangeProblem.java
@@ -21,9 +21,12 @@ public class CoinChangeProblem {
 
 		int tabulation = tabulation(denoms, denoms.length, amount);
 
+		int optimizedTabulation = optimizedTabulation(denoms, denoms.length, amount);
+
 		System.out.println("bruteForce -> " + bruteForce);
 		System.out.println("memoizatin -> " + memoization);
 		System.out.println("tabulation -> " + tabulation);
+		System.out.println("OptimizedTabulation -> " + optimizedTabulation);
 		
 	}
 
@@ -40,12 +43,76 @@ public class CoinChangeProblem {
 
 	public static int memoization(int[] denmos, int denomLength, int amount) {
 
-		return 0;
+		int[][] lookupTable = new int[denomLength][amount+1];
+
+		for(int i=0; i< denomLength ; i++) 
+			for(int j=0; j<amount+1; j++)
+				lookupTable[i][j] = 0;
+
+		return memoization(lookupTable, denmos, denomLength, amount) ;
+	}
+
+	private static int memoization(int[][] lookupTable, int[] denoms, int denomLength, int amount){
+
+		// This is where the Change matches with the exact amount
+		if(amount==0) return 1;
+		if(denomLength <=0 || amount <0) return 0;
+		if(lookupTable[denomLength-1][amount] != 0) return lookupTable[denomLength-1][amount];
+		
+		lookupTable[denomLength-1][amount] = memoization(lookupTable, denoms, denomLength-1, amount) + memoization(lookupTable, denoms, denomLength, amount - denoms[denomLength-1]);
+		
+		return lookupTable[denomLength-1][amount];
 	}
 
 	public static int tabulation(int[] denoms, int denomLength, int amount) {
 
-		return 0;
+		int x,y;		
+
+		// Edge Case
+		if(amount <=0 || denomLength <=0) return 0;
+		
+		// Lookup Table
+		int[][] lookupTable = new int[amount+1][denomLength];
+
+		// initialize the first row
+		for(int i=0; i<denomLength; i++) lookupTable[0][i]=1;
+
+		// Fill the remaining entries 
+		// bottom up manner
+		for(int i=1; i<amount+1; i++) {
+			for(int j=0; j<denomLength; j++) {
+				
+				x=(i-denoms[j] >=0)? lookupTable[i-denoms[j]][j]:0;
+				y=(j>=1)? lookupTable[i][j-1]:0;
+				lookupTable[i][j]=x+y;
+			}
+		}
+
+		return lookupTable[amount][denomLength-1];
+	}
+
+	public static int optimizedTabulation(int[] denoms, int denomLength, int amount) {
+
+		if(amount <= 0 || denomLength <= 0) return 0;
+
+		int[] lookupTable = new int[amount+1];
+
+		for(int eachElement: lookupTable) eachElement=0;
+
+		lookupTable[0] =1;
+
+		System.out.println(Arrays.stream(lookupTable).mapToObj(String::valueOf).collect(Collectors.joining(",","{","}")));
+
+		for(int i=0; i<denomLength; i++) {
+
+			for(int j=denoms[i]; j<=amount; j++) {
+
+				lookupTable[j] += lookupTable[j-denoms[i]];
+
+				System.out.println(Arrays.stream(lookupTable).mapToObj(String::valueOf).collect(Collectors.joining(",","{","}")));
+			}
+		}
+		return lookupTable[amount];
 	}
 }
 
