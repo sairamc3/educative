2d9dab203d20ac3bf2d68c6100e2f4d6776fba4e
Sairam Chekuri <sairamc3@gmail.com>
2024-04-23 12:00:03 +0530

    Adding the divide and conquer approach for the problem

--- divide_and_conquer/PeakElement/PeakElement.java
+++ divide_and_conquer/PeakElement/PeakElement.java
@@ -14,6 +14,45 @@ public class PeakElement {
 							.collect(Collectors.joining(",","{","}")));
 		
 		System.out.println("Peak element: " + peakElementIndex);
+
+		// Efficient approach
+		// Using divide and conquer
+		int peakElmentIndexEfficient = findPeakByDivideAndConquer(given);
+
+		System.out.println("Peak Element by Divide and conquer: " + peakElmentIndexEfficient);
+	}
+
+	private static int findPeakByDivideAndConquer(int[] given) {
+
+		int length = given.length;
+
+		return findPeakRecursive(0, length-1, length, given);
+	}
+
+	/**
+	 * This function is based on recursive binary search algorithm,
+	 * returning the index of the peak element in the given array
+	 */
+	private static int findPeakRecursive(int low, int high, int size, int[] given) {
+
+		// Just like binary search
+		// Find the middle element
+		int middle = low + (high - low)/2 ;
+
+		// If the nighbours of the middle element exists
+		// compare them with the middle element
+		if((middle == size-1 || given[middle+1] <= given[middle])
+				&& (middle == 0 || given[middle-1] <= given[middle])) return middle;
+
+		// If the left nighbour of the middle element is greater,
+		// The peak element must be in the left subarray
+		else if(given[middle-1] > given[middle] && middle > 0) 
+
+			return findPeakRecursive(low, middle-1, size, given);
+
+		// If the right nighbour of the middle element is greater,
+		// The peak element must be in the right subarray
+		else return findPeakRecursive(middle + 1, high, size, given);
 	}
 
 	/** 
